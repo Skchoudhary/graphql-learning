d8bc634246d661d498b501ba8dd7a9aaa416efaf
Sandeep Choudhary <sandeepchoudhary1507@gmail.com>
2024-08-29 10:12:58 +0530

    Chapter-5: Add the Mongo db connection

--- index.js
+++ index.js
@@ -5,59 +5,31 @@ const { readFileSync } = require('fs')
 const { GraphQLScalarType } = require('graphql')
 const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 const resolvers = require('./resolvers')
-
-var _id = 0
-var photos = [
-  {
-    "id": "1",
-    "name": "Dropping the Heart Chute",
-    "description": "The heart chute is one of my favorite chutes",
-    "category": "ACTION",
-    "githubUser": "gPlake",
-	  "created": "3-28-1977"
-  },
-  {
-    "id": "2",
-    "name": "Enjoying the sunshine",
-    "category": "SELFIE",
-    "githubUser": "sSchmidt",
-	  "created": "1-2-1985"
-  },
-  {
-    "id": "3",
-    "name": "Gunbarrel 25",
-    "description": "25 laps on gunbarrel today",
-    "category": "LANDSCAPE",
-    "githubUser": "sSchmidt",
-	  "created": "2018-04-15T19:09:57.308Z"
-  }
-]
-var users = [
-  { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
-  { "githubLogin": "gPlake", "name": "Glen Plake" },
-  { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
-]
-
-var tags = [
-    { "photoID": "1", "userID": "gPlake" },
-    { "photoID": "2", "userID": "sSchmidt" },
-    { "photoID": "2", "userID": "mHattrup" },
-    { "photoID": "2", "userID": "gPlake" }
-]
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
 
 
-var app = express()
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
+async function start() {
+	const app = express()
+	const MONGO_DB = process.env.DB_HOST
 
-})
+	const client = await MongoClient.connect (
+		MONGO_DB,
+	)
+	const db = client.db()
+	const context = { db }
+	const server = new ApolloServer ({ typeDefs, resolvers, context })
+	await server.start()
+	server.applyMiddleware({ app })
+	app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
+	app.get('/playground', expressPlayground({ endpoint: '/graphql'}))
 
-server.applyMiddleware({app})
-app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
-app.listen({ port: 4000}, () => console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+	app.listen({ port: 4000 }, () => 
+		console.log(
+			`GraphQL Server runnig at http://localhost:4000${server.graphqlPath}`
+			)
+			)
+}
 
-server
-    .listen()
-    .then(({url}) => console.log(`GraphQl Service running on ${url}`))
+start()
+
--- resolvers/index.js
+++ resolvers/index.js
@@ -1,7 +1,20 @@
+const { GraphQLScalarType } = require('graphql')
+
 const resolvers = {
     Query: {
-        totalPhotos: () => photos.length,
-            allPhotos: () => photos
+        totalPhotos: (parent, args, {db}) => db.collection('photos')
+	    .estimatedDocumentCount(),
+
+	    allPhotos: (parent, args, { db }) => db.collection('photos')
+	    .find()
+	    .toArray(),
+	    totalUsers: (parent, args, { db }) => db.collection('users')
+	    .estiamtedDoucmentCount(),
+	    allUsers: (parent, args, { db }) => 
+	    db.collection('users')
+	    .find()
+	    .toArray()
+
     },
 
     Mutation: {
